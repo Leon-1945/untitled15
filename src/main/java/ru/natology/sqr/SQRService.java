ad9b87634ddc09237d722be4895e4da77842247d
Leon1945 <Muravlevevgeniy@yandex.ru>
2023-02-21 22:34:34 +0400

    Initial commit

---
+++ src/main/java/ru/natology/sqr/SQRService.java
@@ -0,0 +1,15 @@
+package ru.natology.sqr;
+public class SQRService {
+    public int calc(int min, int max) {
+        int result = 0;
+        for (int i = 10; i <= 99; i++) {
+            int Square = i * i;
+            if (Square >= min) {
+            if (Square <= max) {
+                  result++;
+            }
+            }
+        }
+        return result;
+}
+}
---
+++ src/test/java/ru/natology/sqr/SQRServiceTest.java
@@ -0,0 +1,71 @@
+package ru.natology.sqr;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SQRServiceTest {
+
+    @Test
+    public void testWhenFewSquaresFound() {
+        SQRService service = new SQRService();
+
+        int actual = service.calc(200, 300);
+        int expected = 3;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testWhenFewSquaresFound2() {
+        SQRService service = new SQRService();
+
+        int actual = service.calc(200, 200);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testWhenFewSquaresFound3() {
+        SQRService service = new SQRService();
+
+        int actual = service.calc(100, 120);
+        int expected = 1;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testWhenFewSquaresFound4() {
+        SQRService service = new SQRService();
+
+        int actual = service.calc(10, 99);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testWhenFewSquaresFound5() {
+        SQRService service = new SQRService();
+
+        int actual = service.calc(9650, 9801);
+        int expected = 1;
+
+        Assertions.assertEquals(expected, actual);
+}
+    @Test
+    public void testWhenFewSquaresFound6() {
+        SQRService service = new SQRService();
+
+        int actual = service.calc(300, 200);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+}
+}
+
